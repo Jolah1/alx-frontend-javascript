479f19f64f346f8fc551ebe239747347479a3006
okunlade Jolade <jokunlade@gmail.com>
2023-01-23 23:25:08 +0100

    advanced

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+export default function createIteratorObject(report) {
+    let allEmployees = [];
+    for (const item of Object.values(report.allEmployees)) {
+      allEmployees = [
+        ...allEmployees,
+        ...item,
+      ];
+    }
+    return allEmployees;
+  }
+  
