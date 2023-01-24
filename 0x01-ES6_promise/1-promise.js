ed89eb8429b1a13151289eff80cad16a052d010d
okunlade Jolade <jokunlade@gmail.com>
2023-01-24 16:37:16 +0100

    return promise all

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function (boolean) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+    if (boolean === true) {
+      resolve(object);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
